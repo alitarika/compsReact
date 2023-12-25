3c35e5ef515fcd102e223fae16dc8d29ae019370
alitarika <alitarikalacam@gmail.com>
2023-12-25 10:06:12 +0300

    Merge remote changes

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { GoBell } from "react-icons/go";
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button
+          primary
+          onMouseOver={() => console.log("moused")}
+          onClick={() => console.log("clickedS")}
+          className="mb-5"
+        >
+          Click here
+        </Button>
+      </div>
+      <div>
+        <Button secondary>
+          <GoBell /> Saasd
+        </Button>
+      </div>
+      <div>
+        <Button primary outline rounded>
+          <GoBell />
+          Saasd
+        </Button>
+      </div>
+      <div>
+        <Button danger rounded outline>
+          Saasd
+        </Button>
+      </div>
+      <div>
+        <Button secondary> Sasdasd </Button>
+      </div>
+    </div>
+  );
+}
+export default App;
