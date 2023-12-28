afaab7f5569952e52d4826bf1f8c52a84035dcb6
alitarika <alitarikalacam@gmail.com>
2023-12-28 23:31:25 +0300

    modal component with createPortal

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -9,12 +9,30 @@ function ModalPage() {
     setShowModal(true);
   };
 
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
+    <div>
+      <Button onClick={handleClose} primary>
+        I Accept
+      </Button>
+    </div>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is an important agreement for you to accept</p>
+    </Modal>
+  );
+
   return (
     <div>
       <Button onClick={handleClick} danger rounded>
         Open Modal
       </Button>
-      {showModal && <Modal />}
+      {showModal && modal}
     </div>
   );
 }
