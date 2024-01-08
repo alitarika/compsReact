cd6a3a5be4cb4eefe7060678c7a2ad5c2b5d8711
alitarika <alitarikalacam@gmail.com>
2024-01-08 15:41:34 +0300

    CounterPage initialized without custom hook

---
+++ src/pages/CounterPage.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import Button from "../components/Button";
+
+function CounterPage({ initialCount }) {
+  const [count, setCount] = useState(initialCount);
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h1>Count is {count}</h1>
+      <Button primary onClick={handleClick}>
+        Increment
+      </Button>
+      <Button danger onClick={handleDecrement}>
+        Decrement
+      </Button>
+    </div>
+  );
+}
+
+export default CounterPage;
